8e42a20c1bc425340411be07ba20d981ba18bbd2
Saul Nunez <saul.nunez99@gmail.com>
2025-10-27 21:51:57 -0600

    Remove application daos as they will be separated to another service

--- Models/DAO/ApplicationDao.cs
+++ Models/DAO/ApplicationDao.cs
@@ -5,8 +5,5 @@ public record ApplicationDao
     public required string Name { get; init; }
     public required string Description { get; init; }
     public required List<string> PhotoUrls { get; init; }
-    public AndroidApplicationDao? AndroidPackages { get; init; }
-    public WindowsApplicationDao? WindowsApplication { get; init; }
-    public MacOsApplicationDao? MacOsApplication { get; init; }
     public List<CommentDao> Comments { get; init; } = [];
 }
